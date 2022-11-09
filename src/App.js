ffdbb07b8bf43c75e2b549feeb9c7e769f865fd5
Ankaj Goyal <goyal.ankaj.tft@outlook.com>
2022-11-09 13:33:35 +0530

    react-mapbox

--- src/App.js
+++ src/App.js
@@ -1,23 +1,85 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import 'mapbox-gl/dist/mapbox-gl.css';
+import "./App.css";
+import Map, {
+  NavigationControl,
+  FullscreenControl,
+  GeolocateControl,
+  Layer,
+  Source,
+  Popup,
+} from "react-map-gl";
+import { clusterCountLayer, clusterLayer, unclusteredPointLayer } from "./layer";
+import logo from "./logo.svg";
 
-function App() {
+
+const App = () => {
+  const [viewState, setViewState] = useState({
+    longitude: -100,
+    latitude: 40,
+    zoom: 3.5
+  });
+
+  const [data, setData] = useState(null);
+  const [pointData, setPointData] = useState(null);
+
+  useEffect(() => {
+    fetch("https://docs.mapbox.com/mapbox-gl-js/assets/earthquakes.geojson")
+      .then((res) => res.json())
+      .then((data) => setData(data))
+  },[])
+
+  const onMapPointClickHandler = (event) => {
+    event.preventDefault();
+    const feature = event.features[0];
+    if(feature?.properties?.id) {
+      setPointData({
+        ...feature.properties,
+        ...feature.geometry
+      })
+    }
+  }
+
+  const onPopUpCloseHandler = () => setPointData(null)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <Map
+        mapboxAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
+        style={{
+          width: "100vw",
+          height: "100vh"
+        }}
+        {...viewState}
+        onMove={evt => setViewState(evt.viewState)}
+        mapStyle="mapbox://styles/mapbox/streets-v11"
+        interactiveLayerIds={[clusterLayer.id, unclusteredPointLayer.id, clusterCountLayer.id]}
+        onMouseEnter={onMapPointClickHandler}
+        onMouseLeave={onPopUpCloseHandler}
+      >
+        <Source
+          id="earthquakes"
+          type="geojson"
+          data={data}
+          cluster={true}
+          clusterMaxZoom={22}
+          clusterRadius={50}
         >
-          Learn React
-        </a>
-      </header>
+          <Layer {...clusterLayer} />
+          <Layer {...clusterCountLayer} />
+          <Layer {...unclusteredPointLayer} />
+        </Source>
+        <NavigationControl position="top-right" />
+        <FullscreenControl />
+        <GeolocateControl />
+        {
+          pointData && 
+          <Popup longitude={pointData.coordinates[0]} latitude={pointData.coordinates[1]} onClose={onPopUpCloseHandler}>
+            <div>{pointData.id}</div>
+            <img src={logo} alt="logo" />
+          </Popup>
+        }
+      </Map>
     </div>
   );
 }
